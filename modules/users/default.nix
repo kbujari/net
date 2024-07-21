a46fd764f8141b2a719ea0d13da020816917c325
Kleidi Bujari <mail@4kb.net>
2024-07-21 14:51:03 -0400

    .

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -12,7 +12,8 @@
     ];
   };
 
-  users.users.kbujari = {
+  /* users.users.kbujari = {
+    isNormalUser = true;
     extraGroups = [
       "wheel"
       "kvm"
@@ -21,6 +22,6 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDPIOY5gZ86K9cWq2Qa/AhLtNtt0QBQHNM+n2WaeLp2n"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7T2uWJFUu8aFZZgQusGKyEMocb2pKbHLDad2eIJus9"
     ];
-  };
+  }; */
 
 }
