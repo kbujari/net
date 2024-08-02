ccd23f355fef96610f0600dff89d2d48784243db
Kleidi Bujari <mail@4kb.net>
2024-08-02 13:12:24 -0400

    .

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -3,16 +3,19 @@
     acceptTerms = true;
     defaults = {
       email = "dev+certs@4kb.net";
-    };
-
-    certs."4kb.net" = {
-      extraDomainNames = [ "*.4kb.net" ];
-      directory = "/persist/certs/acme/4kb.net";
       dnsProvider = "porkbun";
       credentialsFile = "/run/porkbun";
       reloadServices = [ "nginx" ];
     };
+
+    certs."4kb.net" = {
+      extraDomainNames = [ "*.4kb.net" ];
+    };
   };
+
+  systemd.tmpfiles.rules = [
+    "L /var/lib/acme - - - - /persist/certs/acme"
+  ];
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   users.users.nginx.extraGroups = [ "acme" ];
