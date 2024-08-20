619e9c6f5e20f0ddde0f36b09675e8a856ca5915
Kleidi Bujari <mail@4kb.net>
2024-08-20 04:11:03 -0400

    .

--- xnet/monitoring/prometheus.nix
+++ xnet/monitoring/prometheus.nix
@@ -2,14 +2,14 @@
   services.prometheus = {
     enable = true;
     globalConfig.scrape_interval = "15s";
-    scrapeConfigs = [{
-      job_name = "node";
-      relabel_configs = [{
-        source_labels = [ "__address__" ];
-        regex = "(.*):[0-9]+";
-        target_label = "instance";
-        replacement = "$1";
-      }];
-    }];
+    # scrapeConfigs = [{
+    #   job_name = "node";
+    #   relabel_configs = [{
+    #     source_labels = [ "__address__" ];
+    #     regex = "(.*):[0-9]+";
+    #     target_label = "instance";
+    #     replacement = "$1";
+    #   }];
+    # }];
   };
 }
