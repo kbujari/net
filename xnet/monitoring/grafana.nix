75d8c876549e318a49b47150e364ec0fdca00a47
Kleidi Bujari <mail@4kb.net>
2024-08-20 04:44:16 -0400

    .

--- xnet/monitoring/grafana.nix
+++ xnet/monitoring/grafana.nix
@@ -16,15 +16,36 @@ in
       access = "proxy";
       editable = false;
     }];
-  };
 
-  services.grafana.provision.dashboards.settings.providers = [
-    {
-      name = "Node Exprter Full";
+    dashboards.settings.providers = [{
+      name = "Fetched Dashboards";
       type = "file";
-      options.path = fet.outPath;
-    }
-  ];
+      disableDeletion = true;
+      updateIntervalSeconds = 3600; # Optional: update every hour
+      options = {
+        path = pkgs.symlinkJoin {
+          name = "grafana-dashboards";
+          paths = [
+            (pkgs.fetchurl {
+              name = "node-exporter-dashboard.json";
+              url = "https://grafana.com/api/dashboards/1860/revisions/27/download";
+              sha256 = "0s3wmxw66xc8bvg28q2g2swklfj7n1q2ngbqbrx86w859w3jkimc";
+            })
+            (pkgs.fetchurl {
+              name = "systemd-dashboard.json";
+              url = "https://grafana.com/api/dashboards/13474/revisions/1/download";
+              sha256 = "1z1fvxqgp47gqx7s76ajlf4hc0mxqcm2npidkwd1gqy2s3ys1v9k";
+            })
+            (pkgs.fetchurl {
+              name = "prometheus-2-0-overview.json";
+              url = "https://grafana.com/api/dashboards/11074/revisions/9/download";
+              sha256 = "1h6n2gj0j0p3z1lmdvc33wplg5a0wb4n0q39mvawy3fqi4pmsbqq";
+            })
+          ];
+        };
+      };
+    }];
+  };
 
   networking.firewall.allowedTCPPorts = [ 3000 ];
   services.grafana = {
