508448971e2e4d96d4fc70dabecdbb6d0d5cae38
Kleidi Bujari <mail@4kb.net>
2024-08-20 04:45:31 -0400

    .

--- xnet/monitoring/grafana.nix
+++ xnet/monitoring/grafana.nix
@@ -1,9 +1,6 @@
 { config, lib, pkgs, ... }:
 let
-  fet = pkgs.fetchurl {
-    url = "https://grafana.com/api/dashboards/1860/revisions/37/download";
-    hash = "sha256-1DE1aaanRHHeCOMWDGdOS1wBXxOF84UXAjJzT5Ek6mM=";
-  };
+  fet = pkgs.fetchurl { };
 
 in
 {
@@ -27,19 +24,8 @@ in
           name = "grafana-dashboards";
           paths = [
             (pkgs.fetchurl {
-              name = "node-exporter-dashboard.json";
-              url = "https://grafana.com/api/dashboards/1860/revisions/27/download";
-              sha256 = "0s3wmxw66xc8bvg28q2g2swklfj7n1q2ngbqbrx86w859w3jkimc";
-            })
-            (pkgs.fetchurl {
-              name = "systemd-dashboard.json";
-              url = "https://grafana.com/api/dashboards/13474/revisions/1/download";
-              sha256 = "1z1fvxqgp47gqx7s76ajlf4hc0mxqcm2npidkwd1gqy2s3ys1v9k";
-            })
-            (pkgs.fetchurl {
-              name = "prometheus-2-0-overview.json";
-              url = "https://grafana.com/api/dashboards/11074/revisions/9/download";
-              sha256 = "1h6n2gj0j0p3z1lmdvc33wplg5a0wb4n0q39mvawy3fqi4pmsbqq";
+              url = "https://grafana.com/api/dashboards/1860/revisions/37/download";
+              hash = "sha256-1DE1aaanRHHeCOMWDGdOS1wBXxOF84UXAjJzT5Ek6mM=";
             })
           ];
         };
