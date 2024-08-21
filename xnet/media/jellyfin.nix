f958ce67ba95ef5a9e0444b8d3c8bf7161e72086
Kleidi Bujari <mail@4kb.net>
2024-08-20 20:15:46 -0400

    .

--- xnet/media/jellyfin.nix
+++ xnet/media/jellyfin.nix
@@ -1,7 +1,24 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
   services.jellyfin = {
     enable = true;
   };
+
+  environment.systemPackages = with pkgs; [ jellyfin jellyfin-web jellyfin-ffmpeg ];
 
   services.nginx.virtualHosts."mov.web.4kb.net" = {
     locations."/" = {
